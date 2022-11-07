912b851dd94cc9b879109e35f54465ab340d9748
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-07 19:38:52 +0100

    scaffolding and import external components add provide to set an image inside a div

---
+++ src/components/AppHero.vue
@@ -0,0 +1,35 @@
+<script>
+    export default{
+        data(){
+            return {
+                immagine:'vue.svg'
+            }
+        },methods: {
+            getImagePath(){
+                return new URL(`../assets/${this.immagine}`, import.meta.url).href;
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
+
+<template>
+  <div class="jumbotron">
+    <img :src="getImagePath()" alt="">
+  </div>
+
+</template>
+
+<style scoped>
+    .jumbotron{
+        width: 100%;
+        height: 30vh;
+    }
+
+    .jumbotron img{
+        width: 100%;
+        height: 100%;
+    }
+</style>
